9441ae0dbf43898a4240320e5d6fd4f5fbcfde02
Trinh Thien Bao <trinhthienbao@Trinhs-MacBook-Pro.local>
2024-04-24 22:32:07 +0700

    commit day 1

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+@app.route("/<name>")
+def index(name):
+    return render_template('index.html', content=name)
 
 @app.route("/home")
 def home():
     return 'This is home'
 
+@app.route("/<user>")
+def login(user):
+    return f"Hello {user}"
+
+@app.route("/book")
+def book():
+    return 'This is books'
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug = True)
